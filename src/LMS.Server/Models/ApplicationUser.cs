be1d4b1f2cd447e0a3f8a7ccf025ebbe6eac16d2
Angel Neychev <angel.neychev@gmail.com>
2023-04-22 12:16:55 +0300

    Update ApplicationRole and ApplicationUser

--- src/LMS.Server/Models/ApplicationUser.cs
+++ src/LMS.Server/Models/ApplicationUser.cs
@@ -1,6 +1,9 @@
 ﻿namespace LMS.Server.Models
 {
     using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+
+    using static LMS.Server.Models.Common.Validation.ApplicationUserValidation;
 
     public class ApplicationUser : IdentityUser
     {
@@ -10,7 +13,28 @@
             this.Roles = new HashSet<IdentityUserRole<string>>();
             this.Claims = new HashSet<IdentityUserClaim<string>>();
             this.Logins = new HashSet<IdentityUserLogin<string>>();
+            this.IsFirstLogin = false;
         }
+
+        [Required]
+        [MaxLength(MaxLengthPersonalIdentificationNumber)]
+        [MinLength(MinLengthPersonalIdentificationNumber)]
+        public string UniqueCitizenshipNumber { get; set; }
+
+        [Required]
+        [MaxLength(MaxLengthFullName)]
+        [MinLength(MinLengthFullName)]
+        public string FullName { get; set; }
+
+        public bool IsFirstLogin { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
 
         public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
 
---
+++ src/LMS.Server/Models/Common/Validation.cs
@@ -0,0 +1,23 @@
+﻿namespace LMS.Server.Models.Common
+{
+    public class Validation
+    {
+        public class ApplicationUserValidation
+        {
+            public const int MaxLengthPersonalIdentificationNumber = 10;
+            public const int MinLengthPersonalIdentificationNumber = 10;
+            public const int MaxLengthFullName = 100;
+            public const int MinLengthFullName = 3;
+        }
+        public class CompanyValidation
+        {
+            public const int MaxLengthCompanyName = 100;
+            public const int MaxLengthCompanyAddress = 200;
+        }
+
+        public class DepartmentValidation
+        {
+            public const int MaxLengthDepartmentName = 100;
+        }
+    }
+}
